fbe7ec3a95852f59dab8a256ac0a25e2ecd2f39b
aucd29 <aucd29@gmail.com>
2015-03-05 17:00:04 +0900

    * revert ui/drag

---
+++ src/com/obigo/f10/ui/drag/DragScroller.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2012 OBIGO Inc. All rights reserved.
+ *             http://www.obigo.com
+ */
+
+package com.obigo.f10.ui.drag;
+
+/**
+ * Handles scrolling while dragging
+ *
+ */
+public interface DragScroller {
+    void scrollLeft();
+    void scrollRight();
+
+//    // WebLauncher added Function
+//    void scrollUp();
+//    void scrollDown();
+}
